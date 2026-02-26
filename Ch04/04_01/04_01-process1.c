fc54789d9a6c31c6ff899427aa1646d1d628be5c
Imran <82314727+uvxv@users.noreply.github.com>
2026-02-26 15:01:13 +0000

    --

--- Ch04/04_01/04_01-process1.c
+++ Ch04/04_01/04_01-process1.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
 
 int main()
 {
     pid_t process;
+    pid_t parent;
 
-    process = getpid();
+    process = getpid(); // current process id
+    parent = getppid();
+
+    kill(process, SIGHUP); // the process will be killed 
+    // but because no handler is designed to catch the return from kill, the kernel excutes teh default handler
+    // and the program hangs up
+
     printf("This program's process ID is %d\n",process);
-
+    printf("The parent process of %d is %d ", process, parent);
     return 0;
 }
--- Ch04/04_02/04_02-fork1.c
+++ Ch04/04_02/04_02-fork1.c
@@ -4,6 +4,7 @@
 int main()
 {
     fork();
+    fork();
     printf("This code's PID is %d\n",getpid());
 
     return 0;
